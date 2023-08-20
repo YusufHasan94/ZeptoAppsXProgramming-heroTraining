9835e76b024819792be601ee4e1bda0c5dc18751
YusufHasan94 <yhpolok@gmail.com>
2023-08-20 23:11:33 +0600

    update session file and add cookies in milestone 2

--- milestone2/php/readme.md
+++ milestone2/php/readme.md
@@ -1,3 +1,5 @@
 1. cover php variable, constant,  data types
 2. cover loop, arrays
 3. solve practice using loop and conditions.  
+4. practice session 
+5. practice cookies 
--- milestone2/php/session.php
+++ milestone2/php/session.php
@@ -1,20 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Session Example</title>
+</head>
+<body>
+    <form method="POST" action="">
+        <label for="input">Enter a number:</label>
+        <input type="text" name="number" id="input">
+        <button type="submit" name="check">Check Condition</button>
+        <button type="submit" name="close">Close Session</button>
+    </form>
+</body>
+</html>
+
+
 <?php
     session_start();
-
-    if(isset($_POST['login'])){
-        if($valid_credentials){
-            $_SESSION['username'] = $_POST['username'];
-            header('Location: dashboard.php');
-            exit;
+    if (isset($_POST['check'])) {
+        $number = $_POST['number'];
+        $printWord = "";
+        if ($number % 2 == 0) {
+            $printWord = "The number $number is even.";
+        } else {
+            $printWord = "The number $number is odd.";
         }
-        else{
-            $error_msg = 'Invalid Credentials';
-        }
-    }
-    if(isset($_POST['logout'])){
+        echo $printWord;
+    } elseif (isset($_POST['close'])) {
+        $printWord = "";
+        session_unset();
         session_destroy();
-        header('Location: login.php');
-        exit;
+        echo "Session closed.";
     }
-
 ?>
